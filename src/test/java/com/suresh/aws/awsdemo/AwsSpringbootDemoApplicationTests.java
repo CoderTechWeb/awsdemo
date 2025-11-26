ab96eb0d09da7c56171e37462b719f580e6bc2f4
Suresh <snarayanaswamy@proofpoint.com>
2025-11-26 23:07:28 +0530

    Initial commit

---
+++ src/test/java/com/suresh/aws/awsdemo/AwsSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh.aws.awsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
